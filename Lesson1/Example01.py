42eb161e85dc6c56c0ce65f560a86d6ed5095fac
sebastiyan <tav-vil@mail.ru>
2021-12-17 12:21:39 +1200

    Задания 1 урока

---
+++ Lesson1/Example01.py
@@ -0,0 +1,13 @@
+a = 5
+b = 10
+c = 15
+print("Переменные a, b, c - ", a, b, c)
+string1 = input("Введите первую строку ")
+number1 = int(input("Введите первое число "))
+string2 = input("Введите вторую строку ")
+number2 = int(input("Введите второе число "))
+string3 = input("Введите третью строку ")
+number3 = int(input("Введите третье число "))
+
+print(
+    "Введеные значения>%10s >%5d %10s> %5d %10s> %5d" % (string1, number1, string2, number2, string3, number3))
