6adfb95855f70ad54fac7716cc64acde3348e61b
sebastiyan <tav-vil@mail.ru>
2022-01-06 12:01:53 +1200

    add files

---
+++ Lesson1/11.py
@@ -0,0 +1,10 @@
+year_off_birthday = 1999
+print(year_off_birthday)
+pet_name = "linky"
+print(pet_name)
+
+user_growth = int(input("Введите свой рост в сантиметрах :"))
+print("Похоже это ", user_growth * 10, "миллиметров." )
+
+user_name = input("Введите своё имя :")
+print("Приятно познакомиться ,", user_name, "!")
